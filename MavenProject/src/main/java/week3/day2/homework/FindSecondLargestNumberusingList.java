63b33b4327ae4bd472f6e551f6384e5eee8daf5c
HP <HP@DESKTOP-KTVBHPS>
2024-03-20 11:20:43 +0530

    SecondLargest integer

---
+++ MavenProject/src/main/java/week3/day2/homework/FindSecondLargestNumberusingList.java
@@ -0,0 +1,34 @@
+package week3.day2.homework;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FindSecondLargestNumberusingList {
+
+	public static void main(String[] args) {
+
+		int[] inputValue = { 3, 2, 11, 4, 6, 7 };
+
+		int x;
+
+		List<Integer> list = new ArrayList<>();
+
+		for (int i = 0; i <= inputValue.length - 1; i++) {
+
+			int input = inputValue[i];
+			list.add(input);
+
+		}
+
+		Collections.sort(list);
+
+		System.out.println("Sorted List " + list);
+
+		list.get(1);
+
+		System.out.println("Second Largerst integer in the List Array is " + list.get(1));
+
+	}
+
+}
